12f7790c0c832a0bb0acf035594bebf615a5eee8
Jeff Fowler <jafowler421@gmail.com>
2026-03-09 14:04:44 -0500

    completed pipeline

--- ml/model.py
+++ ml/model.py
@@ -129,6 +129,7 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+    data_slice = data[data[column_name] == slice_value]
     X_slice, y_slice, _, _ = process_data(
         data_slice,
         categorical_features=categorical_features,
